6f3a2240c42030c51bda12801b12f8b96afb1e1f
Andrew Lee <candrewlee14@gmail.com>
2024-03-26 17:47:01 -0600

    Add BitLinear1B and BitLinear158B classes, work on notebook

---
+++ src/finalnlp/bitnet1.py
@@ -0,0 +1,66 @@
+from torch import Tensor, nn
+from torch.nn import functional as F
+from rmsnorm_torch import RMSNorm
+
+def activation_quant(x: Tensor):
+    """Per token quantization to 8bits. No grouping is needed for quantization
+
+    Args:
+        x (Tensor): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    scale = 127.0 / x.abs().max(dim=-1, keepdim=True).values.clamp_(min=1e-5)
+    y = (x * scale).round().clamp_(-128, 127) / scale
+    return y
+
+def weight_quant(w: Tensor):
+    """
+    Per–tensor quantization to 1 bits. No grouping is needed for quantization.
+    Args:
+    w: a weight tensor with shape [d, k]
+    Returns:
+    u: a quantized weight with shape [d, k]
+    """
+    scale = w.abs().mean()
+    e = w.mean()
+    u = (w - e).sign() * scale
+    return u
+
+class BitLinear1B(nn.Linear):
+    """
+    Custom linear layer with bit quantization.
+
+    Args:
+        dim (int): The input dimension of the layer.
+        training (bool, optional): Whether the layer is in training mode or not. Defaults to False.
+        *args: Variable length argument list.
+        **kwargs: Arbitrary keyword arguments.
+
+    Attributes:
+        dim (int): The input dimension of the layer.
+
+    """
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Forward pass of the BitLinear layer.
+
+        Args:
+            x (Tensor): The input tensor.
+
+        Returns:
+            Tensor: The output tensor.
+
+        """
+        b, s, d = x.shape
+        w = self.weight
+        x_norm = RMSNorm(d)(x)
+
+        # STE using detach
+        x_quant = x_norm + (activation_quant(x_norm) - x_norm).detach()
+        w_quant = w + (weight_quant(w) - w).detach()
+        y = F.linear(x_quant, w_quant)
+        return y
+
--- src/finalnlp/bitnet158.py
+++ src/finalnlp/bitnet158.py
@@ -1,23 +1,68 @@
 import torch.nn as nn
 from rmsnorm_torch import RMSNorm
 import torch.nn.functional as F
+import torch
 
-def activation_quant(x):
-    scale = 127.0 / x.abs().max(dim=-1,keepdim=True).values.clamp_(min=1e-5)
-    y = (x*scale).round().clamp_(-128,127) / scale
+def activation_norm_quant(x: torch.Tensor):
+    """RMSNorm & Per-token quantization to 8 bits. It can be implemented as a fused kernel.
+    Args:
+        x: an activation tensor with shape [n,d]
+    Returns:
+        y: a quantized activation tensor with shape [n,d]
+        scale: a scalar for dequantization with shape [1]
+    """
+    x = RMSNorm(x)
+    scale = 127.0 / x.abs().max(dim=-1, keepdim=True).values.clamp_(min=1e-5)
+    y = (x * scale).round().clamp_(-128, 127)
+    return y, scale
+
+def activation_quant(x: torch.Tensor):
+    """Per-token quantization to 8 bits. No grouping is needed for quantization.
+    Args:
+        x: an activation tensor with shape [n,d]
+    Returns:
+        y: a quantized activation tensor with shape [n,d]
+    """
+    scale = 127.0 / x.abs().max(dim=-1, keepdim=True).values.clamp_(min=1e-5)
+    y = (x * scale).round().clamp_(-128, 127) / scale
     return y
 
-def weight_quant(w):
+def weight_quant(w: torch.Tensor):
+    """Per-tensor quantization to 1.58 bits. No grouping is needed for quantization.
+    Args:
+        w: an weight tensor with shape [d,k]
+    Returns:
+        u: a quantized weight with shape [d,k]
+    """
     scale = 1.0 / w.abs().mean().clamp_(min=1e-5)
-    u = (w*scale).round().clamp_(-1,1) / scale
+    u = (w * scale).round().clamp_(-1, 1) / scale
     return u
 
-class BitLinear(nn.Linear):
+class BitLinear158B(nn.Linear):
+    """
+    """
 
     def forward(self, x):
-        w = self.weight
-        x_norm = RMSNorm(x)
-        x_quant = x_norm + (activation_quant(x_norm) - x_norm).detach()
-        w_quant = w + (weight_quant(w) - w).detach()
-        y = F.linear(x_quant, w_quant)
-        return y
+        """
+        Args:
+            x: an input tensor with shape [n,d]
+        Returns:
+            y: an output tensor with shape [n,d]
+        """
+        if self.training:
+            w = self.weight
+            x_norm = RMSNorm(x)
+            # A trick for implementing Straight-Through-Estimator (STE) using detach()
+            x_quant = x_norm + (activation_quant(x_norm) - x_norm).detach()
+            w_quant = w + (weight_quant(w) - w).detach()
+            y = F.linear(x_quant, w_quant)
+            return y
+        else:
+            w = self.weight  # a 1.58-bit weight tensor with shape [d, k]
+            w_scale = (
+                self.weight_scale
+            )  # a full precision weight scale tensor with shape [1]
+            x_quant, x_scale = activation_norm_quant(x)
+            #y = gemm_lowbit_kernel(x_quant, w) / w_scale / x_scale
+            y = F.linear(x_quant, w) / w_scale / x_scale
+            return y

cf4f2ff4b678c7d2b3f82ad3c9bacfc0a5c66575
Isaac Kletzli <isaackletzli@gmail.com>
2024-04-04 21:24:44 -0600

    started comparing bitnet layer to model without bitnet layer

--- src/finalnlp/replacer.py
+++ src/finalnlp/replacer.py
@@ -57,4 +57,4 @@ def replace_linears_in_pytorch_model(
             )
         else:
             # Recursively apply to child modules
-            replace_linears_in_pytorch_model(module)
+            replace_linears_in_pytorch_model(module, BitLinear)
